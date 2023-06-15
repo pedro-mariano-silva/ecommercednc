4a4167df5a5810f9bc91bc0175fb1d15ad0bcdb8
Pedro Henrique Mariano <pedro_mariano.silva@outlook.com>
2023-06-14 21:20:35 -0300

    Subindo o projeto e-commerce (versão1)

---
+++ src/components/CardProducts/CardProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./index.scss"
+
+const CardProducts = () => {
+  return (
+    <div>CardProducts</div>
+  )
+}
+
+export default CardProducts
---
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function HeaderMenu() {
+  return (
+    <div>HeaderMenu</div>
+  )
+}
+
+export default HeaderMenu
